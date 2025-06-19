84118821311ed0d0cec9c3842bc33b09463082cc
CybulCoder <kamateo1235@gmail.com>
2025-06-19 19:29:17 +0200

    added pre-draw data func

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include"data_utils.h"
 #include"sorting_algorithms.h"
+#include<SDL2/SDL.h>
 
+void draw_state(std::vector<unsigned short>& data, SDL_Renderer* rend, unsigned int red, unsigned int blue){
+	int index = 0;
+	for(int i : data){
+		SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+		SDL_RenderDrawLine(rend, index, 99, index, i);
+		index += 1;
+	}
+}
 
 int main(){
 	std::cout << "Hello!\n";
@@ -12,15 +21,19 @@ int main(){
 
 	data = factory.MakeRandom(100,1,1000);
 
-	SACC::print_pretty(data);
 
-	SACC::Insertion_sort(data);
+ 	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_CreateWindowAndRenderer(100*10, 100*10, 0, &window, &renderer);
+	SDL_RenderSetScale(renderer,10,10); 
 
-	std::cout << "\n\n";
 
-	SACC::print_pretty(data);
-	
+	SDL_SetRenderDrawColor(renderer,0,0,0,255);
+	SDL_RenderClear(renderer);
+	draw_state(data,renderer,0,0);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(10);
 
-	std::cout << "\n\nAnd... That's it...";
+	std::cout << "\n\nAnd... That's it...\n";
 	return 0;
 }
