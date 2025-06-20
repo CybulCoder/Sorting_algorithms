e64dd790c313733ff2624682aa89c75debfc8bed
CybulCoder <kamateo1235@gmail.com>
2025-06-20 22:46:57 +0200

    learning SDL2

--- demo.cpp
+++ demo.cpp
@@ -1,38 +1,62 @@
 #include<iostream>
-#include"data_utils.h"
-#include"sorting_algorithms.h"
+//#include"data_utils.h"
+//#include"sorting_algorithms.h"
 #include<SDL2/SDL.h>
 
-void draw_state(std::vector<unsigned short>& data, SDL_Renderer* rend, unsigned int red, unsigned int blue){
-	int index = 0;
-	for(int i : data){
-		SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
-		SDL_RenderDrawLine(rend, index, 99, index, i);
-		index += 1;
+SDL_Surface* OptimizedSurface(std::string filePath, SDL_Surface* windowSurface)
+{
+	SDL_Surface* optimizedSurface = nullptr;
+	SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
+	if(surface == NULL){
+		std::cout << "ERROR!\n";
 	}
+
+	optimizedSurface = SDL_ConvertSurface(surface, windowSurface->format, 0);
+
+	if(optimizedSurface == NULL){
+		std::cout << "ERROR!\n";
+	}
+
+	SDL_FreeSurface(surface);
+	return optimizedSurface;
 }
 
-int main(){
+int main(int argc, char *argv[]){
 	std::cout << "Hello!\n";
 
-	using T = SACC::T;
-	SACC::RecordFactory factory;
-	std::vector<T> data;
-
-	data = factory.MakeRandom(100,1,1000);
-
-
  	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
-	SDL_CreateWindowAndRenderer(100*10, 100*10, 0, &window, &renderer);
-	SDL_RenderSetScale(renderer,10,10); 
+	SDL_Surface* windowSurface = nullptr;
+	SDL_Surface *currentImage = nullptr;
+	
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("okno", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	windowSurface = SDL_GetWindowSurface(window);
+	currentImage = OptimizedSurface("photo.bmp", windowSurface);
+	
+	SDL_Rect drawingRect;
+	drawingRect.x = drawingRect.y =0;
+	drawingRect.w = 640;
+	drawingRect.h = 480;
+
+	bool isRunning = true;
+	SDL_Event ev;
+
+	while(isRunning){
+		while(SDL_PollEvent(&ev) != 0){
+			if(ev.type == SDL_QUIT){
+				isRunning = false;
+			}
+		}
+		SDL_BlitScaled(currentImage, NULL, windowSurface, &drawingRect);
+		SDL_UpdateWindowSurface(window);
+	}
 
 
-	SDL_SetRenderDrawColor(renderer,0,0,0,255);
-	SDL_RenderClear(renderer);
-	draw_state(data,renderer,0,0);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(10);
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	SDL_FreeSurface(currentImage);
+	currentImage = windowSurface = nullptr;
+	SDL_Quit();
 
 	std::cout << "\n\nAnd... That's it...\n";
 	return 0;
